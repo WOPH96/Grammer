9eaea5fd312bdd67e5d8a84ebff7ccafd72ca8c6
Wonphil Yang <ywonphil@gmail.com>
2022-02-26 00:19:47 +0900

    win

---
+++ cpp/win_cmake/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/ywonp/Documents/study/grammer/cpp/win_cmake
+# Build directory: /mnt/c/Users/ywonp/Documents/study/grammer/cpp/win_cmake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
