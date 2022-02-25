3c0349c3ebed4450ff0ab794ebc50f59d5abf9af
Wonphil Yang <ywonphil@gmail.com>
2022-02-26 00:22:23 +0900

    remove

--- cpp/win_cmake/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/ywonp/Documents/study/grammer/cpp/win_cmake
-# Build directory: /mnt/c/Users/ywonp/Documents/study/grammer/cpp/win_cmake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
