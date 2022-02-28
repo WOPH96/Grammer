77a0a59b7da0917f0b7e8a069c65e02978535373
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-02-28 17:00:24 +0900

    22_02_28

--- cpp/cmake_used/main.cpp
+++ cpp/cmake_used/main.cpp
@@ -1,59 +1,7 @@
 #include <iostream>
 
-class Complex
-{
-private:
-    double real, img;
-
-public:
-    Complex(double real, double img) : real(real), img(img) {}
-    Complex(const Complex &c) : real(c.real), img(c.img) {}
-
-    Complex operator+(const Complex &c) const;
-    Complex operator-(const Complex &c) const;
-    Complex operator/(const Complex &c) const;
-    Complex operator*(const Complex &c) const;
-    Complex &operator=(const Complex &c);
-};
-
-Complex Complex::operator+(const Complex &c) const
-{
-    Complex result(real + c.real, img + c.img);
-
-    return result;
-}
-Complex Complex::operator-(const Complex &c) const
-{
-    Complex result(real - c.real, img - c.img);
-
-    return result;
-}
-Complex Complex::operator*(const Complex &c) const
-{
-    Complex result(real * c.real - img * c.img, real * c.img + img * c.real);
-    return result;
-}
-Complex Complex::operator/(const Complex &c) const
-{
-    Complex result(
-        (real * c.real + img * c.img) / (c.real * c.real + c.img * c.img),
-        (img * c.real - real * c.img) / (c.real * c.real + c.img * c.img));
-    return result;
-}
-
-Complex &Complex::operator=(const Complex &c)
-{
-    real = c.real;
-    img = c.img;
-    return *this;
-}
-
 int main()
 {
-    Complex a(1, 3);
-    Complex b(2, 5);
-
-    a + b;
 
     return 0;
 }
