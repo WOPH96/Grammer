9631f95e41999d26a845146d1b22d1bb981ffa40
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-02-28 13:39:38 +0900

    22_02_28

--- cpp/cmake_used/quick_sort/main.cpp
+++ cpp/cmake_used/quick_sort/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
 using namespace std;
 // Quick sort 구현
 // 1 2 5 4 8 3 0 4 2 6 5 2 7 8
@@ -14,10 +17,10 @@ void SWAP(int &a, int &b)
     b = temp;
 }
 
-void printarr(int arr[])
+void printarr(int arr[], int size)
 {
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
@@ -27,12 +30,15 @@ int sorting(int left, int right, int arr[])
     //첫번째 원소가 피벗
     int &pivot = left;
 
+    // 첫번째 원소를 중간값과 교환 *** 중요 !!! ***
+    // SWAP(arr[pivot], arr[(int)((left + right) / 2)]);
+
     // cout << arr[pivot] << endl;
 
     int low = left + 1;
     int high = right;
 
-    cout << "first low : " << low << " high : " << high << endl;
+    // cout << "first low : " << low << " high : " << high << endl;
 
     do
     {
@@ -52,22 +58,22 @@ int sorting(int left, int right, int arr[])
 
         if (low < high)
         {
-            cout << "before_swap \t ";
-            printarr(arr);
+            // cout << "before_swap \t ";
+            // printarr(arr);
             SWAP(arr[low], arr[high]);
-            cout << "after_swap \t ";
-            printarr(arr);
+            // cout << "after_swap \t ";
+            // printarr(arr);
         }
     } while (low < high);
-    cout << "low : " << low << " high : " << high << endl;
+    // cout << "low : " << low << " high : " << high << endl;
 
     if (arr[high] < arr[pivot])
     {
-        cout << "before_swap \t ";
-        printarr(arr);
+        // cout << "before_swap \t ";
+        // printarr(arr);
         SWAP(arr[high], arr[pivot]);
-        cout << "after_swap \t ";
-        printarr(arr);
+        // cout << "after_swap \t ";
+        // printarr(arr);
         return high;
     }
     return pivot;
@@ -80,8 +86,8 @@ void quicksort(int left, int right, int arr[])
     {
         int p = sorting(left, right, arr); // 피벗 위치 반환
 
-        cout << "pivot " << p << endl;
-        // printarr(arr);
+        // cout << "pivot " << p << endl;
+        //  printarr(arr);
 
         quicksort(left, p - 1, arr); // 0 3
         quicksort(p + 1, right, arr);
@@ -90,13 +96,34 @@ void quicksort(int left, int right, int arr[])
 
 int main()
 {
-    // int arr[n] = {5, 3, 8, 4, 9, 1, 6, 2, 7};
+    // int arr[] = {1, 2, 5, 4, 8, 3, 7, 4, 2, 6, 5, 2, 7, 8};
+    // // printarr(arr);
+    // int size = sizeof(arr) / sizeof(arr[0]);
 
-    // quicksort(0, n - 1, arr);
+    int arr[100000];
 
-    int arr[n] = {1, 2, 5, 4, 8, 3, 7, 4, 2, 6, 5, 2, 7, 8};
-    // printarr(arr);
-    quicksort(0, n - 1, arr);
+    srand((int)time(NULL));
+    for (int i = 0; i < 100000; i++)
+    {
+        arr[i] = rand() % 3000000;
+    }
+    int size = 100000;
+
+    clock_t start, finish;
+
+    start = clock();
+    quicksort(0, size - 1, arr);
+    finish = clock();
+    double duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    // printarr(arr, size);
+    cout << duration << "s" << endl;
+
+    start = clock();
+    quicksort(0, size - 1, arr);
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    // printarr(arr, size);
+    cout << duration << "s" << endl;
 
     return 0;
 }
