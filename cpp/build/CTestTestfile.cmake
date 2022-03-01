d4bf013b2d38e59216609c7da24ddc4096f27b93
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-02 08:55:30 +0900

    22_03_02

---
+++ cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yang-wonphil/Desktop/study/grammer/cpp
+# Build directory: /Users/yang-wonphil/Desktop/study/grammer/cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
