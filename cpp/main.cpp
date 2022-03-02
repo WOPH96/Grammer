4fd69037229d9ae6e7e3ee2e6da5a81fda1eee98
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-02 18:44:51 +0900

    .

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,25 +5,12 @@ using std::endl;
 using std::string;
 
 template <typename T>
-struct Compare
+void print(T arg)
 {
-    bool operator()(const T &a, const T &b) const { return a < b; }
-};
-
-template <typename T, typename Comp = Compare<T> >
-T min(T &a, T &b)
-{
-    Comp comp;
-    if (comp(a, b))
-        return a;
-    else
-        return b;
-    //   return a < b;
 }
 
 int main()
 {
-    int x = 3;
-    int y = 5;
-    cout << min(y, x) << endl;
+
+    return 0;
 }
