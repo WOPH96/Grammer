e0d44dbe7bfa081deac165b2d1e911e2f661ad94
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-03 13:30:02 +0900

    .

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,29 +2,7 @@
 #include <deque>
 using namespace std;
 
-template <typename T>
-void print_deque(deque<T> dq)
-{
-    for (const T &itr : dq)
-        cout << itr << " ";
-    cout << endl;
-}
-
 int main()
 {
-    deque<int> dq;
-    dq.push_back(10);
-    dq.push_back(20);
-    dq.push_back(30);
-    dq.push_front(40);
-    dq.push_front(50);
-
-    cout << "초기 dq :" << endl;
-    print_deque(dq);
-
-    cout << "맨앞 원소 제거 " << endl;
-    dq.pop_front();
-    print_deque(dq);
-
     return 0;
 }
