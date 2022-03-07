f19dafaeacd32cce4a2bd88f58ec688d0b206baf
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-07 14:58:19 +0900

    람다 표현식 예시

--- python/dongbinna/practice.py
+++ python/dongbinna/practice.py
@@ -1,5 +1,18 @@
-# 응용
+# 람다 표현식 예시
 
-data = list(map(lambda x: x+1, map(int, input().split())))
+array = [('홍길동', 50), ('이순신', 80), ('양원필', 75)]
 
-print(data)
+print(array)
+
+print(sorted(array, key=lambda x: x[1]))
+print(array)
+
+array.sort(reverse=True, key=lambda x: x[1])
+print(array)
+
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [6, 7, 8, 9, 10]
+
+result = map(lambda a, b: a+b, lst1, lst2)  # 각각의 원소에 함수를 적용하고싶을 떄 map 사용
+
+print(list(result))
