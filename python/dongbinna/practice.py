662fd0dc7b44824cb5303e726f29d9f57812462e
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-11 19:32:38 +0900

    퀵소트

--- python/dongbinna/practice.py
+++ python/dongbinna/practice.py
@@ -1,15 +1,33 @@
 # 정렬이란 데이터를 특정한 기준에 나열하는 것
 
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-# 선택 정렬 (selection sort)
+# 퀵 정렬 (Quick sort)
 
-for i in range(len(array)):
-    min_idx = i
-    for j in range(i+1, len(array)):
-        if array[j] < array[min_idx]:
-            min_idx = j
 
-    array[i], array[min_idx] = array[min_idx], array[i]
+def quick_sort(arr, left, right):
+    if left >= right:
+        return
+    pivot = left  # pivot 인덱스 저장
+    end = right
+    left += 1
+    arr[pivot], arr[(right+left)//2] = arr[(right+left)//2], arr[pivot]
+    while(left < right):
+        while(left <= right and arr[left] < arr[pivot]):
+            left += 1
+        while(left <= right and arr[right] > arr[pivot]):
+            right -= 1
+        if(left < right):
+            arr[left], arr[right] = arr[right], arr[left]
+            print(arr)
+    if arr[pivot] > arr[right]:
+        arr[pivot], arr[right] = arr[right], arr[pivot]
+    pivot, right = right, pivot
+    print(arr, "pivot : ", pivot)
 
-print(array)
+    quick_sort(arr, right, pivot-1)
+    quick_sort(arr, pivot+1, end)
+
+
+quick_sort(arr, 0, len(arr)-1)
+print(arr)
