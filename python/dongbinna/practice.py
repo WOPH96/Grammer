987b594f7d8eac22d714842fa9ff16e23eca7279
Wonphil Yang <ywonphil@gmail.com>
2022-03-15 22:08:30 +0900

    피보나치 다이나믹 프로그래밍 탑다다운

--- python/dongbinna/practice.py
+++ python/dongbinna/practice.py
@@ -2,16 +2,28 @@
 
 # 1 1 2 3 5 8 13 21 ...
 
-# 일반적인 피보나치 수열, 계산한 결과도 다시 계산하여 사용
+# 다이나믹 프로그래밍 사용
+# DP table을 이용한다
 
-#fibo(40) 시 소요시간 : 24s
+# 탑다운
+
 
 def fibo(x):
 
     if x<=2:
+        DP[x]=1
         return 1
     
-    return fibo(x-1)+fibo(x-2)
+    if DP[x] != 0 :
+        return DP[x]
 
-print(fibo(40))
+    DP[x] = fibo(x-1)+fibo(x-2)
+
+    return DP[x]
+
+n = int(input())
+
+DP = [0]*(n+1)
+
+print(fibo(n))
  
