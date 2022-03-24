e647fcf71d5599d6f358a7b08643b27074f3712d
Wonphil Yang <ywonphil@gmail.com>
2022-03-24 22:26:55 +0900

    .

--- python/dongbinna/practice.py
+++ python/dongbinna/practice.py
@@ -1,44 +1,31 @@
-#효율적인 화폐 구성
+#금광 캐기
 
-#많은 걸 최대한 많이 사용..하면 안되나?
+def maxmining(gold):
 
-#16원 => 3*4 + 2*2 = 6개
+    
 
-#   16원
-#  각 금액별로 최소화폐를 적는다
-# d[i] = i원을 만들기 위한 최소화폐
-# d[1] = 10000
-# d[2] = 1
-# d[3] = 1
-# d[4] = d[2]+d[2] =2
-# d[5] = d[3]+d[2] =2
-# d[6] = min(d[4]+d[2],d[3]+d[3])
-# d[7] = min(d[3]+d[7-3],d[2]+d[7-4])
-# d[i] = min(d[M]+d[i-M],...,d[m]+d[i-m])
-# --> min보다는 M으로 했을때 되길 바라보자
+    return g
+
+T = int(input())
 
 
-n,m = map(int,input().split())
+tmp = []
+gold= []
+for _ in range(T):
+    n,m = map(int,input().split())
+    
+    tmp.extend(list(map(int,input().split())))
+    k=0
+    for i in range(n):
+        lst =[]
+        for j in range(m):
+            lst.append(tmp[k])
+            k+=1
+        gold.append(lst)  #다른 방법이 있을 듯 한데..
 
-coins =[]
-for _ in range(n):
-    coins.append(int(input()))
-
-d = [10000]* 101
-
-coins.sort(reverse=True)
-
-for coin in coins:
-    d[coin] = 1
+    print(maxmining(gold))    
+    
 
 
-for i in range(max(coins)+1,m+1):
-    for coin in coins: # 큰값부터 체크
-        d[i] = d[coin]+d[i-coin] # 
-        if d[i] < 10000: break # 제대로 개수 취한거
-
-if d[m]>=10000:
-    d[m] = -1
-
-print("최소화폐개수 = ",d[m])
-
+print(gold)
+        
