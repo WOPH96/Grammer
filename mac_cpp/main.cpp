39f4e339f78809faf352b942eb27ac164fe70830
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-25 11:36:51 +0900

    .

--- mac_cpp/main.cpp
+++ mac_cpp/main.cpp
@@ -1,21 +1,10 @@
 #include <algorithm>
-#include <functional>
-#include <iostream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-template <typename Iter>
-void print(Iter begin, Iter end)
-{
-    while (begin != end)
-    {
-        std::cout << "[" << *begin << "] ";
-        begin++;
-    }
-    std::cout << std::endl;
-}
 int main()
 {
     std::vector<int> vec;
