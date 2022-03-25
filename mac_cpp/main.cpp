bb71e3dbbd1418e1092a0bcb2067fe8723a300a5
양원필 <yang-wonphil@Phils-MacBookPro.local>
2022-03-25 16:39:43 +0900

    cmp,lambda테스트

--- mac_cpp/main.cpp
+++ mac_cpp/main.cpp
@@ -5,9 +5,34 @@
 
 using namespace std;
 
+// comp test
+
+template <typename T1, typename T2>
+void printv(vector<pair<T1, T2>> v)
+{
+    for (auto x : v)
+    {
+        cout << x.first << x.second << endl;
+    }
+    cout << endl;
+};
+
 int main()
 {
-    std::vector<int> a;
 
+    vector<pair<int, int>> a;
+
+    a.push_back(pair<int, int>(1, 3));
+    a.push_back(make_pair(4, 2));
+    a.push_back(make_pair(6, 8));
+    a.push_back(make_pair(3, 7));
+    a.push_back(make_pair(1, 9));
+
+    printv(a);
+    sort(a.begin(), a.end());
+    printv(a);
+    sort(a.begin(), a.end(), [](pair<int, int> p1, pair<int, int> p2)
+         { return p1.second > p2.second; });
+    printv(a);
     // cout << vec[0].push_back(3) << endl;
 }
